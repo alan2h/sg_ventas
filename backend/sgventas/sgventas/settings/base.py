db116c557504b8037895ae33454af45dc403790c
Alan Beck <beckalan303@gmail.com>
2023-11-16 17:46:03 -0300

    nuevo cambio

--- backend/sgventas/sgventas/settings/base.py
+++ backend/sgventas/sgventas/settings/base.py
@@ -116,7 +116,7 @@ DATABASES = {
         'PORT': env('PORT')
     }
 }
-# este es la cong de la apis
+# este es la cong de la apis asdasd
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
